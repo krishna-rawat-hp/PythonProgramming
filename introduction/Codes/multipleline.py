b2172ce1638e076fa4b5f563e94463303711f2dd
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-07-28 17:13:31 +0530

    Introduction Of Python

---
+++ introduction/Codes/multipleline.py
@@ -0,0 +1,3 @@
+print("Hello, This is a Computer Programmer.\n\n \t I can write python programs")
+print("---------------------------------------------------")
+print("Python is a very easier language to read and write.")
