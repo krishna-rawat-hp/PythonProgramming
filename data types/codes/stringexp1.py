dd4823d1816de74b6b29acf0673e1ca846e455eb
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-07-30 17:33:53 +0530

    datatype folder commit2

---
+++ data types/codes/stringexp1.py
@@ -0,0 +1,8 @@
+str1 = "hello python " #string str1
+str2 = "how are you" #string str2
+
+print(str1[0:2]) #printing first two character using slice operator
+print(str2[4:7]) #print are word using string slice
+print(str1[6])   #printing dth character of the str1
+print(str1 + str2) #printing the concatenation of str1 and str2
+print(str1 * 2)  #printing the string twice
