856af710b196c5b1494bb3b9cca2786d78812137
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-07-30 17:40:11 +0530

    datatype folder commit5

---
+++ data types/codes/dictionaryexp.py
@@ -0,0 +1,11 @@
+d = {1:'Jimmy', 2:'Alex', 3:'john', 4:'mike'}
+
+# Accesing value using keys
+print("1st name is "+d[1])
+print("2nd name is "+ d[4])
+
+# Printing dictionary
+print (d)
+
+print (d.keys())
+print (d.values())
