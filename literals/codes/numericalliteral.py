f2ca67cddd0f586bf44c32e784c31288577a3b5d
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-02 15:34:26 +0530

    numericalliteral file

---
+++ literals/codes/numericalliteral.py
@@ -0,0 +1,15 @@
+x = 0b10100 # Binary literal, here 0b represent binary
+y = 100 #decimal literal
+z = 0o215 #Octal literal, here 0o represent octal
+u = 0x12d #Hexadecimal literal, here 0x represent hexadecimal
+
+#float literal
+float1 = 100.5
+float2 = 1.5e2
+
+#complex literal
+a = 5+3.14j
+
+print(x, y, z, u)
+print(float1, float2)
+print(a, a.imag, a.real)
