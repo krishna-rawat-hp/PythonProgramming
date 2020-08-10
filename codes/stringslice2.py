cfbb7fce5d79cd164a1f9d7d44e5d3cb58b0c2a6
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-10 11:49:34 +0530

    stringslice2.py file

---
+++ codes/stringslice2.py
@@ -0,0 +1,14 @@
+str = "HELLO"
+
+# print H E L L O
+print(str[-5])
+print(str[-4])
+print(str[-3])
+print(str[-2])
+print(str[-1])
+
+# print hello
+print(str[-5:])
+
+# print he
+print(str[-5:-3])
