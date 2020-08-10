79b0c9b8a4c9234a26c0298b82697ee5c13a3550
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-10 11:27:47 +0530

    string1.py file

---
+++ codes/string1.py
@@ -0,0 +1,12 @@
+# string using sinle quote's
+str1 = 'Hello Python'
+print("String using single quote's: ",str1)
+
+# string using double quote's
+str2 = "Hello Python"
+print("String Using double quote's: ",str2)
+
+# using triple quoted string
+str3 = ''' Hello this is python programming.
+                              python is best language ever.'''
+print("Using triple quoted string: ",str3)
