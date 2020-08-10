b2b2a8979299998658c4102b2ac19d7f23e1997d
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-10 11:38:00 +0530

    stringslice1.py file

--- codes/stringindex.py
+++ codes/stringindex.py
@@ -3,6 +3,6 @@ str = "HELLO"
 print(str[0])
 print(str[1])
 print(str[2])
-print(str[3])
+print(str[3])   
 print(str[4])
 print(str[5]) # error string index out of range
---
+++ codes/stringslice1.py
@@ -0,0 +1,13 @@
+str = "HELLO"
+
+# print hello using slice
+print(str[:])
+
+# print he using slice
+print(str[:2])
+
+# print ell using slice
+print(str[1:4])
+
+# print llo using slice
+print(str[2:])
