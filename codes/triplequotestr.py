9ac1169182a4e8b06339f00edd21ddf0107d647a
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-11 10:22:34 +0530

    triplequotestr.py file

---
+++ codes/triplequotestr.py
@@ -0,0 +1,10 @@
+str = '''hello, "What's going on?"'''
+str1 = "hello, 'What\'s going on?'"
+str2 = 'hello, "What\'s going on?"'
+str3 = "hello, \"What's going on?\""
+
+print(str)
+print(str1)
+print(str2)
+print(str3)
+print("\a")
