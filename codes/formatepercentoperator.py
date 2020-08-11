21a0d0725e1d63b75056fd73383fea2d9c39af03
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-11 10:38:30 +0530

    formatepercentoperator.py file

---
+++ codes/formatepercentoperator.py
@@ -0,0 +1,5 @@
+intval = 23
+floatval = 15.50
+strval = "String"
+
+print("Hii I am Integer....... My Value is %d\nHii I am Float.......... My Value is %.2f\nHii I am String........ My Value is %s"%(intval,floatval,strval))
