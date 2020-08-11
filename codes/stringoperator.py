18617853f7df82eab1a39a5e83c49dff2ed50ede
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-11 10:11:16 +0530

    stringoperator.py file

---
+++ codes/stringoperator.py
@@ -0,0 +1,13 @@
+str = "Hello"
+str1 = " world"
+
+print(str*3)
+print(str+str1)
+print(str[: ])
+print(str[4])
+print(str[2:4])
+print('w' in str)
+print('wo' not in str)
+print(r'c:\\python4')
+print(R'c:\\python5')
+print("This is a string: %s,%s"%(str,str1))
