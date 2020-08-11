6df0efe56904716d5a714372f35322f46b2408ff
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-11 10:32:25 +0530

    formatmethodstr.py file

---
+++ codes/formatmethodstr.py
@@ -0,0 +1,4 @@
+print("{} and {} are brother.".format("ashik", "aashish"))
+print("{1} and {0} are great cricket player. ".format("Virat", "Rohit"))
+print("{a}, {b} and {c} are friends. ".format(a="hemant", b="shalu", c="krishna"))
+print("{} is mirror image of {}.".format(32,23))
