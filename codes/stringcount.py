ff8e435e63b8f7b1c5957e32ead987eb3cccaf41
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-23 14:31:39 +0530

    stringcount.py file

---
+++ codes/stringcount.py
@@ -0,0 +1,11 @@
+# String count method in python
+
+# Example-1 With single parameter
+s1 = "krishna kant rawat"
+print("Occurences:", s1.count('k'))
+
+# Example-2 with double parameter
+print("Occurences:", s1.count('a',7))
+
+# Example-3 with triple parameter
+print("Occurences:", s1.count('a',2,16))
