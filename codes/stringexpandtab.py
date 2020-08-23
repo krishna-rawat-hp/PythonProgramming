4a085261d4004bcca75c69ea66e9c9f20ff9eccd
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-23 15:39:52 +0530

    stringexpandtabs.py file

---
+++ codes/stringexpandtab.py
@@ -0,0 +1,15 @@
+# String expandtab method in python
+
+# Example-1 default tab size
+s1 = "Krishna \t Kant \t Rawat"
+print(s1)
+print(s1.expandtabs())
+
+# Example-2 tab size modified 1
+print(s1.expandtabs(1))
+
+# Example-3 tab size modified 2
+print(s1.expandtabs(2))
+
+# Example-4 tab size modified 3
+print(s1.expandtabs(3))
