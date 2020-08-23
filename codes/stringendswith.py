def61a3d87966520fcb51431eb32dec1aa4a6b46
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-23 14:56:19 +0530

    stringendswith.py file

---
+++ codes/stringendswith.py
@@ -0,0 +1,11 @@
+# String endswith method in python
+
+# Example-1 with single parameter
+s1 = "Hello this is python."
+print(s1.endswith("."))
+
+# Example-2 with double parameter
+print(s1.endswith("is",10))
+
+# Example-3 with triple parameter
+print(s1.endswith("is",0,13))
