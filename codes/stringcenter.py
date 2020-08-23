0b52be7fbab57f86f93cdd716e68209786473ba5
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-23 14:21:08 +0530

    stringcenter.py file

---
+++ codes/stringcenter.py
@@ -0,0 +1,13 @@
+# String center method in python
+
+# Example 1 without fill attribute
+str1 = "Krishna Kant Rawat"
+str2 = str1.center(30)
+print("String 1:",str1)
+print("String 2:",str2)
+
+# Example 2 with fill attribute
+str3 = " Krishna Kant Rawat "
+str4 = str3.center(30,'-')
+print("Old String:",str3)
+print("New String:",str4)
