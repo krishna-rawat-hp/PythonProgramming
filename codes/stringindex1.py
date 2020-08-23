b0c910f956bb3f4d5953b67d25e66f3558cac36e
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-23 16:16:28 +0530

    stringindex.py file

---
+++ codes/stringindex1.py
@@ -0,0 +1,15 @@
+# String index method in python
+
+# Example-1 single parameter
+str1 = "Hello This is krishna kant rawat"
+print(str1.index("is"))
+
+# Example-2 double parameter
+print(str1.index("krishna",10))
+
+# Example-3 triple parameter
+print(str1.index("kant",22,28))
+
+# Example-4 index not found
+print(str1.index("krs"))
+
