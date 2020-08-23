c87f98b7fd27340ba6c3940722f5e1d4b8ef2434
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-23 13:52:23 +0530

    stringcapitalize.py file

---
+++ codes/stringcapitalize.py
@@ -0,0 +1,19 @@
+# string capitalize method
+
+# 1. simple string
+str1 = "python"
+str11 = str1.capitalize()
+print("Old String:",str1)
+print("New String:",str11)
+
+# 2. if string's first latter is capital
+str2 = "Python"
+str22 = str2.capitalize()
+print("Old String:",str2)
+print("New String:",str22)
+
+# 3. if string's first latter is non-character
+str3 = "1-python"
+str33 = str3.capitalize()
+print("Old String:",str3)
+print("New String:",str33)
