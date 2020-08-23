4e378e1b618b661ad78b80bf37a4304d13600241
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-23 14:10:44 +0530

    stringcasefold.py file

---
+++ codes/stringcasefold.py
@@ -0,0 +1,13 @@
+# string casefold method in python
+
+# Example 1
+s1 = "jAvaTPoiNT"
+s2 = s1.casefold()
+print("Old String:",s1)
+print("New String:",s2)
+
+# Example 2
+s3 = "!!PyTHon#"
+s4 = s3.casefold()
+print("Old String:",s3)
+print("New String:",s4)
