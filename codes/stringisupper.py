117fd8d0c4e61c6e7a574cca75ac8ba796a58245
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-24 11:53:47 +0530

    stringisupper.py file

---
+++ codes/stringisupper.py
@@ -0,0 +1,13 @@
+# String isupper method in python
+
+# Example-1 simple uppercase string
+str1 = "PYTHON PROGRAMMING"
+print(str1.isupper())
+
+# Example-2 mixed case string
+str2 = "Hello WORLD"
+print(str2.isupper())
+
+# Example-3 special, numeric and uppercase string
+str3 = "134 #$% PYTHON"
+print(str3.isupper())
