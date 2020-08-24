1f9208a0a72a8b65fc2fd5af3781992e6559c2a4
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-24 11:04:02 +0530

    stringisidentifier.py file

---
+++ codes/stringisidentifier.py
@@ -0,0 +1,21 @@
+# String isidentifier method in python
+
+# Example-1 simple string
+str1 = "abc"
+print(str1.isidentifier())
+
+# Example-2 string start with digit
+str2 = "12Krishna"
+print(str2.isidentifier())
+
+# Example-3 string start with underscore(_)
+str3 = "_krishna"
+print(str3.isidentifier())
+
+# Example-4 string alphanumeric
+str4 = "krishna123"
+print(str4.isidentifier())
+
+# Example-5 special character
+str5 = "abc#"
+print(str5.isidentifier())
