c42021e7cbd0db2490e6bf224f38805d089720e2
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-24 11:24:14 +0530

    stringisprintable.py file

---
+++ codes/stringisprintable.py
@@ -0,0 +1,13 @@
+# String isprintable mey=thod in python
+
+# Example-1 simple string
+str1 = "hello world"
+print(str1.isprintable())
+
+# Example-2 \n in string
+str2 = "hello \n world"
+print(str2.isprintable())
+
+# Example-3 \t in string
+str3 = "hello \t world"
+print(str3.isprintable()) 
