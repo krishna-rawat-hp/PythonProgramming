c8b7aa1c5d0679488ef9cd19e9fc733f5ae248e8
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-24 12:04:30 +0530

    stringistitle.py file

---
+++ codes/stringistitle.py
@@ -0,0 +1,13 @@
+# String istitle method in python
+
+# Example-1 title cased string
+str1 = "Krishna Kant Rawat"
+print(str1.istitle())
+
+# Example-2 normal string
+str2 = "Krishna kant rawat"
+print(str2.istitle())
+
+# Example-3 spacial, numeric characters
+str3 = "123 #$% 56"
+print(str3.istitle())
