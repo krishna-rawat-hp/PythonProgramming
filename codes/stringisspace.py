5c9c7f1e2411117fd9275c3eed09487a4d5df5ed
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-24 11:59:46 +0530

    stringisspace.py file

---
+++ codes/stringisspace.py
@@ -0,0 +1,17 @@
+# String isspace method in python
+
+# Example-1 blank white spaced string
+str1= " "
+print(str1.isspace())
+
+# Example-2 only one space between two strings
+str2 = "Krishna Rawat"
+print(str2.isspace())
+
+# Example-3 multi space in string
+str3 = "krishna Kant rawat"
+print(str3.isspace())
+
+# Example-4 \n and \t
+str4 = "\n \t \n"
+print(str4.isspace())
