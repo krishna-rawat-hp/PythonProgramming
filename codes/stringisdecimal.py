ef44c81719871bfe65df3e6dd1abbcc2c9cf1e81
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-24 10:53:16 +0530

    stringisdecimal.py file

---
+++ codes/stringisdecimal.py
@@ -0,0 +1,17 @@
+# String isdecimal method in python
+
+# Example-1 decimal string
+str1 = "12345"
+print(str1.isdecimal())
+
+# Example-2 alphabnumeric string
+str2 = "Krishna12"
+print(str2.isdecimal())
+
+# Example-3 float string
+str3 = "2.30"
+print(str3.isdecimal())
+
+# Example-4 special character
+str4 = "!@#$%"
+print(str4.isdecimal())
