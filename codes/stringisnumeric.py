7f738b94c8e5188672e384182bf48b94d76c3866
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-24 11:18:01 +0530

    stringisnumeric.py file

---
+++ codes/stringisnumeric.py
@@ -0,0 +1,13 @@
+# String isnumeric method in python
+
+# Example-1 numeric string
+str1 = "12345"
+print(str1.isnumeric())
+
+# Example-2 alphanumeric string
+str2 = "krishna1234"
+print(str2.isnumeric())
+
+# Example-3 numeric with special character string
+str3 = "12345-56778-90122"
+print(str3.isnumeric())
