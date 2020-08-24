0e4d6eab1655e21242be8ae3bc0db69ae3d1af6a
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-24 10:44:25 +0530

    stringisalpha.py file

---
+++ codes/stringisalpha.py
@@ -0,0 +1,17 @@
+# String isalpha method in python
+
+# Example-1 with alphabet
+str1 = "Krishna"
+print(str1.isalpha())
+
+# Example-2 with alpha numeric 
+str2 = "Krishna1234"
+print(str2.isalpha())
+
+# Example-3 with spaces
+str3 = "Krishna Kant Rawat"
+print(str3.isalpha())
+
+# Example-4 with special characters
+str4 = "Krishna$Kant@Rawat"
+print(str4.isalpha())
