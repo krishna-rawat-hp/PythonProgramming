86c2ef3f32f0172028bac079b809845bfae6a55d
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-24 11:12:59 +0530

    stringislower.py file

---
+++ codes/stringislower.py
@@ -0,0 +1,13 @@
+# String islower method in python
+
+# Example-1 lowercase string
+str1 = "krishna kant rawat"
+print(str1.islower())
+
+# Example-2 lowercase string with digit and special character
+str2 = "krishna 8319******"
+print(str2.islower())
+
+# Example-3 camelcase string and uppercase string
+str3 = "Krishna Kant RAWAT"
+print(str3.islower())
