32f4bfca545e476899a32be42f31b0b11e55c694
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-24 10:58:07 +0530

    stringisdigit.py file

---
+++ codes/stringisdigit.py
@@ -0,0 +1,15 @@
+# String isdigit method in python
+
+# Example-1 digit string
+str1 = "12345"
+print(str1.isdigit())
+
+# Example-2 alpha digit string
+str2 = "krishna12345"
+print(str2.isdigit())
+
+# Example-3 digit with special character
+str3 = "123-456-678"
+print(str3.isdigit())
+
+
