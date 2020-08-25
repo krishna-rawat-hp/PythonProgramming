da605820542fabe7329780ef16fea92964e76074
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-25 11:11:45 +0530

    stringpartition.py file

---
+++ codes/stringpartition.py
@@ -0,0 +1,14 @@
+# String partition method in python
+
+# Example-1 space partition
+str1 = "Krishna kant rawat"
+print(str1.partition(" "))
+
+# Example-2 delimeter partition
+str2 = "hello this is python programming"
+delm = "is"
+print(str2.partition(delm))
+
+# Example-3 not found delm
+str3 = "krishna kant rawat"
+print(str3.partition("no"))
