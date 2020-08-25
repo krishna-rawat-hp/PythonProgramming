0c07f25c8b401c2f714bee6cdd529ae1f1f640c3
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-25 10:50:56 +0530

    stringlower.py file

---
+++ codes/stringlower.py
@@ -0,0 +1,21 @@
+# String lower method in python
+
+# Example-1 simple string
+str = "Krishna kant Rawat"
+print("old string:",str)
+print("new string:",str.lower())
+
+# Example-2 lowercase string
+str1 = "krishna kant rawat"
+print("old string:",str1)
+print("new string:",str1.lower())
+
+# Example-3 numeric string
+str2 = "1234krishna"
+print("old string:",str2)
+print("new string:",str2.lower())
+
+# Example-4 special symbol string
+str3 = "!@#$krishna"
+print("old string:",str3)
+print("new string:",str3.lower())
