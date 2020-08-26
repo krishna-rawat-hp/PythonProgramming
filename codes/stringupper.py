070f3c4f0241e7aae59d4bc42f884414cfd47b35
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-26 12:13:45 +0530

    stringupper.py file

---
+++ codes/stringupper.py
@@ -0,0 +1,16 @@
+# String upper method in python 
+
+# Example-1 simple string
+str1 = "krishna kant rawat"
+print("old:",str1)
+print("new:",str1.upper())
+
+# Example-2 numeric string
+str2 = "124351Krishna"
+print("old:",str2)
+print("new:",str2.upper())
+
+# Example-3 special character
+str3 = " !@#$% kant rawat"
+print("old:",str3)
+print("new:",str3.upper())
