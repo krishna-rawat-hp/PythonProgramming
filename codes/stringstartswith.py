8401a4b7dbb6adfaa62c6411bb3cfcb63131c1d1
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-26 10:34:29 +0530

    stringstartswith.py file

---
+++ codes/stringstartswith.py
@@ -0,0 +1,13 @@
+# String startswith method in python
+# Example-1 single parameter
+str1 = "Hello python"
+print(str1.startswith("Hello"))
+
+# Example-2 is not start
+print(str1.startswith("python"))
+
+# Example-3 double parameter
+print(str1.startswith("py",6))
+
+# Example-4 triple parameter
+print(str1.startswith("pyth",6,11))
