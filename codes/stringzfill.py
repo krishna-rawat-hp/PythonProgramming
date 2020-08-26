79eb9d26ced86e03105783a55cd5f30332204e6d
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-26 12:17:48 +0530

    stringzfill.py file

---
+++ codes/stringzfill.py
@@ -0,0 +1,15 @@
+# String zfill method in python
+
+# Example-1 simple
+str1 = "krishna"
+print(str1.zfill(10))
+
+# Example-2 +, - sign
+str2 = "+65346"
+str3 = "-7453"
+print(str2.zfill(10))
+print(str3.zfill(10))
+
+# Example-3 less size
+str4 = "krishna kant rawat"
+print(str4.zfill(5))
