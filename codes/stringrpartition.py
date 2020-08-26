814057d5a9482795b464561ef2adecc4e0fdfad3
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-26 12:23:45 +0530

    stringrpartition.py file

---
+++ codes/stringrpartition.py
@@ -0,0 +1,14 @@
+# string rpartition method in python
+
+# Example-1 simple partition
+str1 = "python is a programming language"
+print(str1.rpartition('a'))
+
+# Example-2 with sep
+str2 = "python is a programming language"
+print(str2.rpartition('is'))
+
+# Example-3 not found seprator
+print(str1.rpartition("not"))
+
+
