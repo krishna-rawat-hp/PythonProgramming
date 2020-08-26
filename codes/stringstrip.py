702399359035bc6631d298284e50023264506cf3
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-26 10:38:31 +0530

    stringstrip.py file

---
+++ codes/stringstrip.py
@@ -0,0 +1,11 @@
+# String strip method in python
+
+# Example-1 default strip
+str1 = "    krishna kant rawat    "
+print("Old string:",str1)
+print("New string:",str1.strip())
+
+# Example-2 with delimeter
+str2 = "$$$$-Krishna Kant Rawat-$$$$"
+print("Old string:",str2)
+print("New string:",str2.strip("$"))
