3510830426e2e97e6a031c63826e49d541d741c2
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-26 10:54:02 +0530

    stringtranslate.py file

---
+++ codes/stringtranslate.py
@@ -0,0 +1,10 @@
+# String translate method in python
+
+# Python translate() method   
+# Declaring table and variables  
+table = { 97 : 103, 111 : 112, 117 : None }  
+str = "Hello javatpoint"  
+# Calling function  
+str2 = str.translate(table)  
+# Displaying result  
+print(str2)  
