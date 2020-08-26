43fdf46c545257a13fb0b74d134984928da75df8
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-26 10:17:39 +0530

    stringsplit.py file

---
+++ codes/stringsplit.py
@@ -0,0 +1,11 @@
+# String split method in python
+
+# Example-1 simple split
+str1 = "python is a programming language"
+print(str1.split())
+
+# Example-2 split using words
+print(str1.split('a'))
+
+# Example-3 split with maximum split parameter
+print(str1.split('a',3))
