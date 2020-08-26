b9a84637cb4be406c1f3d56ae24e5b54ee7d3070
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-26 10:46:08 +0530

    stringtitle.py file

---
+++ codes/stringtitle.py
@@ -0,0 +1,16 @@
+# String title method in python
+
+# Example-1 all lowercase
+str1 = "krishna kant rawat"
+print("old:",str1)
+print("new:",str1.title())
+
+# Example-1 all lowercase
+str2 = "KRISHNA KANT RAWAT"
+print("old:",str2)
+print("new:",str2.title())
+
+# Example-1 title case
+str3 = "Krishna Kant Rawat"
+print("old:",str3)
+print("new:",str3.title())
