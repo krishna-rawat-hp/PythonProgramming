6c0acdf4ce62f67bd74c882a4cfca4e3b8bb8a82
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-26 10:42:34 +0530

    stringswapcase.py file

---
+++ codes/stringswapcase.py
@@ -0,0 +1,16 @@
+# String swapcase method in python
+
+# Example-1 
+str1 = "Krishna kANT Rawat"
+print("Before:",str1)
+print("After:",str1.swapcase())
+
+# Example-1 
+str2 = "KRIshNA Kant rawat"
+print("Before:",str2)
+print("After:",str2.swapcase())
+
+# Example-1 
+str3 = "kriSHna kANT RAWAT"
+print("Before:",str3)
+print("After:",str3.swapcase())
