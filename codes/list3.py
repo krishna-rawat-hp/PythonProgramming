8db2aa4a35fe950d2a0b4537b9b7bbf4bf473d6e
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-29 20:02:09 +0530

    list3.py file

---
+++ codes/list3.py
@@ -0,0 +1,10 @@
+# python list example of data to information
+lstemp = ["krishna",101,"India"]
+lstdept1 = ["CS",10]
+lstdept2 = ["IT",11]
+HOD_CS = [10,"Mr.rohit"]
+HOD_IT = [11,"Mr.shelesh"]
+
+print("Name: %s ID: %d Country: %s"%(lstemp[0],lstemp[1],lstemp[2]))
+print("Department 1:\n Name: %s ID: %d \nDepartment 2:\n Name: %s ID: %d"%(lstdept1[0],lstdept1[1],lstdept2[0],lstdept2[1]))
+print("HOD_CS:\n Name: %s ID: %d\nHOD_IT:\n Name: %s ID: %d"%(HOD_CS[1],HOD_CS[0],HOD_IT[1],HOD_IT[0]))
