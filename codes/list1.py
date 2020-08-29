d7480c9997b0ee8bbbee8ae947d4934dca7dc8ef
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-29 18:35:30 +0530

    list1.py file

---
+++ codes/list1.py
@@ -0,0 +1,10 @@
+# Python lists example
+
+# Example-1 lists with type function
+lst1 = ["Krishna",23,"Rawat"]
+lst2 = [1,2,3,4,5,6,7,8,9]
+
+print("list 1:",lst1)
+print("Type :",type(lst1))
+print("list 2:",lst2)
+print("Type :",type(lst2))
