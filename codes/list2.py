183d7803b4544d682ce3b9686cce8b2416c26441
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-08-29 18:40:13 +0530

    list2.py file

---
+++ codes/list2.py
@@ -0,0 +1,10 @@
+# python list example-2 list comparision
+lst1 = [1,2,"krishna",3,4,"Rawat",5]
+lst2 = [1,"krishna",2,3,4,"Rawat",5]
+lst3 = [1,2,"krishna",3,4,"Rawat",5]
+
+print("List1:",lst1)
+print("List2:",lst2)
+print("List3:",lst3)
+print("List1 VS List2:",lst1 == lst2)
+print("List1 VS List3:",lst1 == lst3)
