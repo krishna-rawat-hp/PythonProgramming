bfaf375b92c0216b0fe1c61fd52b57db28c4685b
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-25 13:08:30 +0530

    list5.file added

---
+++ codes/list5.py
@@ -0,0 +1,11 @@
+# Python list slicing using negetive indexing
+
+lst = [1,2,3,4,5,6,7]
+# Example-1 print lists
+print("Way 1: ",lst[:])
+print("Way 2: ",lst[-7:])
+
+# Example-2 print sub list
+print("Way 1: ",lst[-7:-2])
+print("Way 2: ",lst[5:0:-2])
+print("Way 3: ",lst[-6:-1:2])
