c8605230dc39db392963824e94d9b259aca2148b
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-25 13:27:51 +0530

    list6.py file added

---
+++ codes/list6.py
@@ -0,0 +1,15 @@
+# Python list updation
+
+lst = [1,22,33,4,55,66,7,8,99]
+print("List Beafore Updation:",lst) 
+# Example-1 update single value
+lst[1]=2
+lst[2]=3
+
+# Example-2 update with slicing
+lst[4:6] = 5,6
+
+# Examlpe-3 update with negetie indexing
+lst[-1] = 9
+
+print("List After Updation: ", lst) 
