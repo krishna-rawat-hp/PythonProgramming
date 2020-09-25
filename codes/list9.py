7660d4019b0199596f509a035300c6fd93289c15
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-25 14:40:06 +0530

    list9.py file added

---
+++ codes/list9.py
@@ -0,0 +1,9 @@
+# Python list removing element
+
+lst = [1,2,3,4,5,6,7]
+print("List before removing elements: ",lst)
+# Example-1 removing element in list
+lst.remove(1)
+lst.remove(5)
+
+print("List after removing element: ",lst)
