a2bdc68ef45ecfd83d421a4faf1e1c5542a62472
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-25 14:49:06 +0530

    list10.py file added

---
+++ codes/list10.py
@@ -0,0 +1,17 @@
+# Python list Built-in function
+
+lst1 = [1,2,3,4,5]
+# Example-1 length of list
+print("Length of list: ",len(lst1))
+
+# Example-2 min value in list
+print("Minimum value in list: ",min(lst1))
+
+# Example-3 Max value in list
+print("Maximum value in list: ",max(lst1))
+
+# Example-4 list() method in python lists 
+str = "Krishna"
+lststr = list(str)
+print("simple string: ",type(str), str)
+print("List of string: ", type(lststr), lststr)
