1baa7440397ef32cb9f1506142103236d9bd59dd
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-25 12:59:11 +0530

    list4.file added

--- codes/list4.py
+++ codes/list4.py
@@ -2,4 +2,17 @@
 
 # Example-1 
 lst = [1,2,3,4,5,6,7]
-print("List:",)
+print("First element of List:",lst[0])
+print("Second element of List:",lst[1])
+print("Third element of List:",lst[2])
+print("Fourth element of List:",lst[3])
+print("Fifth element of List:",lst[4])
+
+
+# Example-2 print list using slice
+print("Way 1:",lst[0:7])
+print("Way 2:",lst[:])
+
+# Example-3 print sublists
+print("Print 2 to 5:", lst[1:5])
+print("Print 2, 4 and 6:", lst[1:6:2])
