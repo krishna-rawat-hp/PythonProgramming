7ae5863a75d2d55bf553fe5e15a3047201d912f1
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-25 13:36:59 +0530

    list7.py file added

---
+++ codes/list7.py
@@ -0,0 +1,22 @@
+# Python list operators
+
+lst1 = [1,2,3,4]
+lst2 = [5,6,7,8]
+
+# Example-1 * Repeatation operator
+print("lst1 * 2 :", lst1*2)
+
+# Example-2 + Concatination operator
+print("lst1 + lst2 : ", lst1+lst2)
+
+# Example-3 (in) exist or not in list operator
+print("2 in lst1 : ",2 in lst1)
+print("2 in lst2 : ",2 in lst2)
+
+# Example-4 list iteration operator
+for i in lst1:
+	print(i)
+
+# Example-5 list length operator 
+print("lst1 length: ", len(lst1))
+print("lst2 length: ", len(lst2))
