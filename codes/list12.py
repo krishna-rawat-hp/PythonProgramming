b07565d1503bf54a33a8a22dafef818c697730e7
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-25 15:06:16 +0530

    list12.py file added

--- codes/list12.py
+++ codes/list12.py
@@ -2,10 +2,10 @@
 
 lst = [11,34,23,76,45,67,23,33]
 print("Before sorting: ",lst)
-# Example-1 ascending order
+# Example-1 ascending order sorting
 lst.sort()
 print("After ascending order sorting: ",lst)
 
-# Example-2 descending order
+# Example-2 descending order sorting
 lst.sort(reverse=True)
 print("After descending order sorting:",lst)
