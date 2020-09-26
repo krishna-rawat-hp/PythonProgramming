7cea363cc80d035995ad8258d865f180a71f4fd2
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-26 16:20:27 +0530

    listvstuple4.py file added

--- codes/listvstuple4.py
+++ codes/listvstuple4.py
@@ -1,1 +1,7 @@
-# Python list VS tuple
+# Python list VS tuple memory use
+
+# example-1
+Tuple = (1,2,3,4,5,6,7,8,9,0,78,34,43,32,43,55,54,212,642,533,43434,54532)  
+List = [1,2,3,4,5,6,7,8,9,0,78,34,43,32,43,55,54,212,642,533,43434,54532]  
+print('Tuple size =', Tuple.__sizeof__())     
+print('List size =', List.__sizeof__())  
