5516036b870958473ddf4629f3948a96ad7d20f6
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-26 15:54:44 +0530

    tuple10.py file added

---
+++ codes/tuple10.py
@@ -0,0 +1,17 @@
+# Python tuple Built-in function
+
+tup1 = (1,2,3,4,5)
+# Example-1 length of tuple
+print("Length of tuple: ",len(tup1))
+
+# Example-2 min value in tuple
+print("Minimum value in tuple: ",min(tup1))
+
+# Example-3 Max value in tuple
+print("Maximum value in tuple: ",max(tup1))
+
+# Example-4 tuple() method in python tuples 
+str = "Krishna"
+tupstr = tuple(str)
+print("simple string: ",type(str), str)
+print("tuple of string: ", type(tupstr), tupstr)
