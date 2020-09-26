8b04b6c79faf3ebb73a09bac86ebad189ab4ab74
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-26 16:11:23 +0530

    listvstuple2.py file added

---
+++ codes/listvstuple2.py
@@ -0,0 +1,15 @@
+# Python list VS tuple program2
+
+# Example-1 print both
+lst = [1,2,3,4,5,6]
+tup = (1,2,3,4,5,6)
+
+print("List Element: ",lst)
+print("Tuple Element: ",tup)
+
+# Example-2 Reassignment in both
+lst[3] = 5
+print("List: ",lst)
+
+tup[3] = 5
+print("Tuple: ",tup)
