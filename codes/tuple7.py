3995e7aef07114f05ba8be66588e0ee68c4bb232
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-26 12:35:51 +0530

    tuple7.py file added

---
+++ codes/tuple7.py
@@ -0,0 +1,11 @@
+# Python tuple slicing using negetive indexing
+
+tup = (1,2,3,4,5,6,7)
+# Example-1 print tuples
+print("Way 1: ",tup[:])
+print("Way 2: ",tup[-7:])
+
+# Example-2 print sub tuple
+print("Way 1: ",tup[-7:-2])
+print("Way 2: ",tup[5:0:-2])
+print("Way 3: ",tup[-6:-1:2])
