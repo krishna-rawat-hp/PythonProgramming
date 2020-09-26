9da5ba177353f191542639009325815bd15b7935
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-26 11:56:32 +0530

    tuple1.py file added

---
+++ codes/tuple1.py
@@ -0,0 +1,16 @@
+# Python Tuple Creation
+
+#Example-1 general way(Mixed variable tuple)
+tup1 = (1,'RAMA',101)
+print("Tuple1 Data: ",tup1)
+print("Tuple1 Type: ",type(tup1))
+
+#Example-2 general way(Single variable tuple)
+tup2 = ("Krishna",'RAMA',"Rawat")
+print("Tuple2 Data: ",tup2)
+print("Tuple2 Type: ",type(tup2))
+
+#Example-3 Special way
+tup3 = 1,3,101
+print("Tuple3 Data: ",tup3)
+print("Tuple3 Type: ",type(tup3))
