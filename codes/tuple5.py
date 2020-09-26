7aac8aaa7ec6012756ad88d7fc6169657ed812ab
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-26 12:28:04 +0530

    tuple5.py file added

---
+++ codes/tuple5.py
@@ -0,0 +1,12 @@
+# Python tuple accessing with indexing
+
+tup = (1,2,3,4,5,6)
+# Example-1 in range indexing
+print("accessing 1,3,4,5 indexes: ",tup[1]," ",tup[3]," ",tup[4]," ",tup[5])
+
+# Example-2 negetive indexing
+print("accessing -5,-3,-2,-1 indexes: ",tup[-5]," ",tup[-3]," ",tup[-2]," ",tup[-1])
+
+# Example-3 out of range indexing
+print("Acees out of range: ",tup[6])
+
