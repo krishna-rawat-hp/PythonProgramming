1796848dc3beca9aa5fdb4f051ccd61aca142550
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-26 12:03:27 +0530

    tuple2.py file added

---
+++ codes/tuple2.py
@@ -0,0 +1,13 @@
+# Python tuple Declare and initialization
+
+# Example-1 Declare empty tuple
+tup1 = ()
+print("tup1 data & type: ",tup1,type(tup1))
+
+# Example-2 initialize tuple with comma (Right way)
+tup2 = ("Hello",)
+print("tup2 data & type: ",tup2,type(tup2))
+
+# Example-3 initialization without comma (Wrong way)
+tup3 = ("Hello")
+print("tup3 data & type: ",tup3,type(tup3))
