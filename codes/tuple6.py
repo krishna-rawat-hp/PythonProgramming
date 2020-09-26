ba7f01e4eee59a0782dca39cd6cf96a3f14bd199
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-26 12:33:43 +0530

    tuple6.py file added

---
+++ codes/tuple6.py
@@ -0,0 +1,18 @@
+# python tuple slicing 
+
+# Example-1 
+tup = (1,2,3,4,5,6,7)
+print("First element of tuple:",tup[0])
+print("Second element of tuple:",tup[1])
+print("Third element of tuple:",tup[2])
+print("Fourth element of tuple:",tup[3])
+print("Fifth element of tuple:",tup[4])
+
+
+# Example-2 print tuples using slice
+print("Way 1:",tup[0:7])
+print("Way 2:",tup[:])
+
+# Example-3 print subtuples
+print("Print 2 to 5:", tup[1:5])
+print("Print 2, 4 and 6:", tup[1:6:2])
