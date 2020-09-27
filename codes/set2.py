de486e1c085000350309a0406237ebd4cced318b
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-27 16:47:45 +0530

    set2.py file added

---
+++ codes/set2.py
@@ -0,0 +1,9 @@
+# Python set mutable and unmutable
+
+# Example-1 set which has immutable element
+set1 = {"krishna",1,2,3,"singh"}
+print("Type & Elements: ",type(set1),set1) 
+
+# Example-2 set which has mutable element
+set2 = {"Krishna",1,2,3,"singh",["kant",2]}
+print("Type & Elements: ",type(set2),set2)
