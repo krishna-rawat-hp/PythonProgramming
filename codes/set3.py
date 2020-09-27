98c40baf27752d16789b53483dabbf4b857c8f43
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-27 16:50:30 +0530

    set3.py file added

---
+++ codes/set3.py
@@ -0,0 +1,9 @@
+# Python set empty sets
+
+# Example-1 with curly braces {}
+set1 = {}
+print("Type : ",type(set1)) # It print type dict because {} shows dictionary
+
+# Example-2 with set() function
+set2 = set()
+print("Type : ",type(set2))
