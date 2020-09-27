44f9fb5231172799bfdfc67c287bb143a88c68ba
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-27 16:54:29 +0530

    set4.py file added

---
+++ codes/set4.py
@@ -0,0 +1,9 @@
+# Python set duplicate testing
+
+# Example-1 integer duplicate
+set1 = {1,2,3,4,2,3,5,6,7,6,5}
+print("Elements: ",set1)
+
+# Example-2 String duplicates
+set2 = {"Krishna","Kant","Rawat","krishna","kant","rawat","Krishna","Kant","Rawat"} # capital small are different (case sensitive)
+print("Elements: ",set2)
