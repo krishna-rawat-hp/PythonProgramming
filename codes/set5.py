9f1f94e880c4fc4bbdc8f75669bf00aa46d47886
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-27 16:59:10 +0530

    set5.py file added

---
+++ codes/set5.py
@@ -0,0 +1,8 @@
+# Python set addition
+
+# Example-1 
+set1 = {"january","february","march","april","may","june","july"}
+print("Before Adding Months: ",set1)
+set1.add("august")
+set1.add("september")
+print("After adding Months: ",set1)
