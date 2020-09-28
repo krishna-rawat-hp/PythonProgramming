6bbc32483c3c79a441cc645303c35a056e85093b
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-28 16:56:15 +0530

    set9.py file added

---
+++ codes/set9.py
@@ -0,0 +1,9 @@
+# Python set operations
+
+# Example-1 intersection of two sets using & operator
+set1 = {"Krishna","Hemant","Raja","Atul","mehta"}
+set2 = {"Rahul","Mohan","Krishna","Hemant"}
+print("Intersection of two sets (&): ",set1&set2)
+
+# Example-2 Intersection of two sets using intersection() method
+print("Intersection of two set (intersection()): ",set1.intersection(set2))
