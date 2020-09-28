935ad5dc3f5009010aea9ad088ae08bc918008ca
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-28 16:54:02 +0530

    set8.py file added

---
+++ codes/set8.py
@@ -0,0 +1,9 @@
+# Python set operations
+
+# Example-1 union of two sets using | operator
+set1 = {"Krishna","Hemant","Raja","Atul","mehta"}
+set2 = {"Rahul","Mohan","Krishna","Hemant"}
+print("Union of two sets (|): ",set1|set2)
+
+# Example-2 Union of two sets using union() method
+print("Union of two set (union()): ",set1.union(set2))
