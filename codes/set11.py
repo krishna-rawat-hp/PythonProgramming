28b8dcc7b76504dc4767a7e87fc8ef752353218f
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-28 17:03:36 +0530

    set11.py file added

---
+++ codes/set11.py
@@ -0,0 +1,9 @@
+# Python set operations
+
+# Example-1 difference of two sets using (-) operator
+set1 = {"Krishna","Hemant","Raja","Atul","mehta"}
+set2 = {"Rahul","Mohan","Krishna","Hemant"}
+print("difference of two sets (-): ",set1-set2)
+
+# Example-2 difference of two sets using difference() method
+print("difference of two set (difference()): ",set1.difference(set2))
