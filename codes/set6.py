bfde6b2cf0fa7a06764f276df084eef955bd07dc
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-09-28 13:44:06 +0530

    set6.py file added

---
+++ codes/set6.py
@@ -0,0 +1,7 @@
+# Python set updation
+
+# Example-1 
+set1 = {"January","February","March","April","May"}
+print("Before set updation: ",set1)
+set1.update(["June","July","August","September"])
+print("After set Updation: ",set1)
