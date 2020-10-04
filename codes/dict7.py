2e5cfac47f5e779d460b13815e002c6e60bee3dc
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-10-04 14:03:40 +0530

    dict7.py file added

---
+++ codes/dict7.py
@@ -0,0 +1,15 @@
+# Python dictionary iteration
+
+dict1 = {"Name":"krishna","age":19,"prof.":"student"}
+
+# Example-1 iterate dictionary keys
+for x in dict1:
+	print(x)
+
+# Example-2 iterate dictionary elements
+for x in dict1:
+	print(dict1[x])
+
+# example-3 iterate dictionary elements
+for x in dict1.values():
+	print(x)
