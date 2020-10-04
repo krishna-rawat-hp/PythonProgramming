52add620f8ec064f39278b078b18c74e2837ab5b
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-10-04 15:17:49 +0530

    dict9.py file added

---
+++ codes/dict9.py
@@ -0,0 +1,17 @@
+# Python dictionary in built dictionary function
+
+dict1 = {"name":"krishna","mname":"kant","lname":"rawat"}
+dict2 = {1:"k",2:"k",3:"r"}
+
+# Example-1 dictionary comparision cmp()
+#print("Comparision :",cmp(dict1,dict2)) it does not work in python 3
+
+# Example-2 len() method
+print("length:",len(dict1))
+
+# Example-3 str() method
+str1 = str(dict2)
+print("string:",str1)
+
+# Example-4 type() method
+print("Type: ",type(dict1))
