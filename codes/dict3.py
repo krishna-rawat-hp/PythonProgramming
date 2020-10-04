b469d160eb37f384e6252b95e387e31b1840c89d
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-10-04 13:05:17 +0530

    dict3.py file added

---
+++ codes/dict3.py
@@ -0,0 +1,15 @@
+# Python dictionary adding element
+
+# Example-1 way-1
+dict1 = {}
+print(" Empty dictionary: ",dict1)
+dict1[0] = "Krishna"
+dict1[2] = "kant"
+dict1[3] = "Rawat"
+
+print("Added Data: ",dict1)
+
+# Example-2 way-2
+dict1["ages"] = 19, 20, 21
+
+print("More elemets: ",dict1)
