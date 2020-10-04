787868ccde24cb3ae98a4801ee0a1b5408d34f15
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-10-04 12:47:18 +0530

    dict1.py file added

---
+++ codes/dict1.py
@@ -0,0 +1,24 @@
+# Python dictionary  creating
+
+# Example-1 creating dictionary by {:}
+dict1 = {"Name":"Krishna","Age":19,"Nationality":"Indian"}
+print(type(dict1))
+print("Data:",dict1)
+print("\n ========================== \n")
+
+# Example-2 Empty dictionary
+dict2 = {}
+print(type(dict2))
+print(dict2)
+print("\n ========================== \n")
+
+# Example-3 using dict() method
+dict3 = dict({1:"krishna",2:"kant",3:"rawat"})
+print(type(dict3))
+print(dict3)
+print("\n ========================== \n")
+
+# Example-4 using dict() method with list and tuple
+dict4 = dict([(1,"krishna"),(2,"kant"),(3,"rawat")]) 
+print(type(dict4))
+print(dict4)
---
+++ codes/dict2.py
@@ -0,0 +1,9 @@
+# python dictionary accessing element
+
+# Example-1 accessing elements
+dict1 = {"name":"krishna","age":19,"surname":"Rawat"}
+print("Printing data...........")
+
+print("Name: ",dict1['name'])
+print("Surname: ",dict1['surname'])
+print("Age: ",dict1['age'])
