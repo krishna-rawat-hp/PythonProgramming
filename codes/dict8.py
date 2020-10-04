05048342995e7c5357bf862083dd1f10162edeb5
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-10-04 14:19:02 +0530

    dict8.py file added

---
+++ codes/dict8.py
@@ -0,0 +1,9 @@
+# Python dictionary duplicate and single element
+
+# Example-1
+dict1 = {'name':'krishna','age':19,'salary':10000,'company':'TCS','name':'krishna'}
+print(dict1)
+
+# Example-2 
+emp = {"name":"krishna","age":19,"salary":1000,"company":"TCS","prof"}
+print("dict2: ",emp)
