67461f8934df59320a9c1c4015c1e234a0b40106
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-10-06 14:31:22 +0530

    function5.py file added

---
+++ codes/function5.py
@@ -0,0 +1,10 @@
+# Python function
+
+# Example-1 User input argument
+def sumofnum(a,b):
+	return a+b
+
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+
+print("Sum :",sumofnum(a,b))
