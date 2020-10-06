572520e2d55c633782e0b47e66c0e560466a0e3f
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-10-06 14:16:10 +0530

    function1.py file added

---
+++ codes/function1.py
@@ -0,0 +1,6 @@
+# Python function define and call
+
+# Example-1 simple
+def hello():
+	print("Hello World!!")
+hello()
---
+++ codes/function2.py
@@ -0,0 +1,9 @@
+# Python function
+
+# Example-1 sum of two numbers
+def sumofnum():
+	a=10
+	b=20
+	c=a+b
+	return c
+print("Sum of two numbers: ",sumofnum())
